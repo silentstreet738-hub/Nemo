a09bf9a643ea75682e8c317be087448f1a07d883
silentstreet738-hub <Silentstreet738@gmail.com>
2026-06-08 04:59:25 +0400

    add voice message capability

--- gizli-cat/server.js
+++ gizli-cat/server.js
@@ -4,32 +4,30 @@ const { Server } = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
+// Səs ötürülməsi zamanı kəsinti olmaması üçün maksimum paket ölçüsünü artırırıq
+const io = new Server(server, {
+    maxHttpBufferSize: 1e7
+});
 
 app.use(express.static(__dirname + '/public'));
 
-// Aktiv istifadəçilərin siyahısı
 const users = {};
 
 io.on('connection', (socket) => {
-    // İstifadəçi adını təyin edəndə
     socket.on('set nickname', (nickname) => {
         users[socket.id] = nickname || 'Anonim';
         io.emit('user list', Object.values(users));
     });
 
-    // Mesaj göndəriləndə
     socket.on('chat message', (data) => {
         data.sender = users[socket.id] || 'Anonim';
         socket.broadcast.emit('chat message', data);
     });
 
-    // Mesaj silinəndə
     socket.on('delete message', (msgId) => {
         io.emit('message deleted', msgId);
     });
 
-    // İstifadəçi çıxanda
     socket.on('disconnect', () => {
         delete users[socket.id];
         io.emit('user list', Object.values(users));

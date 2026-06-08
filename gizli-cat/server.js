45bbae5d81d5e18f7ae4baa1511bd66e1c787e14
silentstreet738-hub <Silentstreet738@gmail.com>
2026-06-08 04:58:03 +0400

    add nickname and contacts layout

--- gizli-cat/server.js
+++ gizli-cat/server.js
@@ -8,16 +8,31 @@ const io = new Server(server);
 
 app.use(express.static(__dirname + '/public'));
 
+// Aktiv istifadəçilərin siyahısı
+const users = {};
+
 io.on('connection', (socket) => {
+    // İstifadəçi adını təyin edəndə
+    socket.on('set nickname', (nickname) => {
+        users[socket.id] = nickname || 'Anonim';
+        io.emit('user list', Object.values(users));
+    });
+
     // Mesaj göndəriləndə
     socket.on('chat message', (data) => {
-        // Gələn mesajı digər istifadəçilərə ID və taymer vaxtı ilə birlikdə ötürür
+        data.sender = users[socket.id] || 'Anonim';
         socket.broadcast.emit('chat message', data);
     });
 
     // Mesaj silinəndə
     socket.on('delete message', (msgId) => {
         io.emit('message deleted', msgId);
+    });
+
+    // İstifadəçi çıxanda
+    socket.on('disconnect', () => {
+        delete users[socket.id];
+        io.emit('user list', Object.values(users));
     });
 });
 
